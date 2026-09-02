9b9926e55c5bae32b07f8102ee858cd318b105d8
jordanstarks-browning <jordan.starks-browning@hippodigital.co.uk>
2026-09-02 16:46:22 +0100

    fix: Increase test coverage

--- Apps/GetAnIdentifier/tests/SUI.GetAnIdentifier.Infrastructure.UnitTests/Services/AuditLogServiceTests.cs
+++ Apps/GetAnIdentifier/tests/SUI.GetAnIdentifier.Infrastructure.UnitTests/Services/AuditLogServiceTests.cs
@@ -3,6 +3,7 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using SUI.GetAnIdentifier.Infrastructure.Services;
 using SUI.GetAnIdentifier.Infrastructure.UnitTests.Utility;
 
@@ -28,7 +29,9 @@ public class AuditLogServiceTests
         // Arrange
         var blobContentInfoResponse = Substitute.For<Response<BlobContentInfo>>();
         var blobClient = Substitute.For<BlobClient>();
-        blobClient.UploadAsync(Arg.Any<Stream>()).Returns(_ => blobContentInfoResponse);
+        blobClient
+            .UploadAsync(Arg.Any<Stream>(), Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns(_ => blobContentInfoResponse);
 
         var blobContainerInfoResponse = Substitute.For<Response<BlobContainerInfo>>();
         _blobContainerClient
@@ -66,7 +69,9 @@ public class AuditLogServiceTests
         // Arrange
         var blobContentInfoResponse = Substitute.For<Response<BlobContentInfo>>();
         var blobClient = Substitute.For<BlobClient>();
-        blobClient.UploadAsync(Arg.Any<Stream>()).Returns(_ => blobContentInfoResponse);
+        blobClient
+            .UploadAsync(Arg.Any<Stream>(), Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns(_ => blobContentInfoResponse);
 
         var blobContainerInfoResponse = Substitute.For<Response<BlobContainerInfo>>();
         _blobContainerClient
@@ -95,5 +100,73 @@ public class AuditLogServiceTests
         await blobClient
             .Received(1)
             .UploadAsync(Arg.Any<Stream>(), overwrite: true, Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task WritAuditLogAsync_HandlesAzureUploadExceptions()
+    {
+        // Arrange
+        var blobClient = Substitute.For<BlobClient>();
+        blobClient
+            .UploadAsync(Arg.Any<Stream>(), Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .ThrowsAsync(new RequestFailedException("Azure service failure"));
+
+        var blobContainerInfoResponse = Substitute.For<Response<BlobContainerInfo>>();
+        _blobContainerClient
+            .CreateIfNotExistsAsync()
+            .ReturnsForAnyArgs(_ => blobContainerInfoResponse);
+        _blobContainerClient.GetBlobClient(Arg.Any<string>()).Returns(x => blobClient);
+
+        var service = new AuditLogService(_logger, _blobContainerClient);
+
+        // Act
+        await service.LogOutgoingResponseAsync(
+            callerId: "test-caller",
+            correlationId: "test-correlation-id",
+            timestamp: DateTimeOffset.UtcNow,
+            statusCode: 200,
+            responseSummary: "Success",
+            cancellationToken: CancellationToken.None
+        );
+
+        // Assert
+        _logger.VerifyLog(
+            LogLevel.Error,
+            "Error occurred in azure services while attempting to write audit logs. Correlation ID: {CorrelationId}"
+        );
+    }
+
+    [Fact]
+    public async Task WritAuditLogAsync_HandlesBlobClientExceptions()
+    {
+        // Arrange
+        var blobContentInfoResponse = Substitute.For<Response<BlobContentInfo>>();
+        var blobClient = Substitute.For<BlobClient>();
+        blobClient
+            .UploadAsync(Arg.Any<Stream>(), Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns(_ => blobContentInfoResponse);
+
+        _blobContainerClient
+            .CreateIfNotExistsAsync()
+            .Throws(new RequestFailedException("Azure service failure"));
+        _blobContainerClient.GetBlobClient(Arg.Any<string>()).Returns(x => blobClient);
+
+        var service = new AuditLogService(_logger, _blobContainerClient);
+
+        // Act
+        await service.LogOutgoingResponseAsync(
+            callerId: "test-caller",
+            correlationId: "test-correlation-id",
+            timestamp: DateTimeOffset.UtcNow,
+            statusCode: 200,
+            responseSummary: "Success",
+            cancellationToken: CancellationToken.None
+        );
+
+        // Assert
+        _logger.VerifyLog(
+            LogLevel.Error,
+            "Error occurred in azure services while attempting to write audit logs. Correlation ID: {CorrelationId}"
+        );
     }
 }
